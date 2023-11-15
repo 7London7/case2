ae01b833c7f88d74dfe9b382475f546dad047730
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-15 12:46:47 +0300

    Update case2.cpp

--- case2.cpp
+++ case2.cpp
@@ -3,9 +3,13 @@
 #include "case2.h"
 using namespace std;
 
-long long itc_math(long long a, long long b, char oper){
+long long itc_math(string str1, string str2, char oper){
+ int i = 0;
+char sum;
  if(oper == '+'){
-    return a + b;
+    while(str1[i] != '\0' && str2[i] != '\0'){
+     sum = (str1[i] - '0') + (str2[i] - '0');
+     i++;
  }
  if(oper == '-'){
     return a - b;
