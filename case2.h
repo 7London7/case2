9e9cca33cef95fad663b94e3aab531859460018e
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-19 21:39:32 +0300

    Update case2.h

--- case2.h
+++ case2.h
@@ -3,6 +3,6 @@
 #include <iostream>
 #include <string>
 using namespace std;
-long long itc_math(long long a, long long b, char oper);
-
+long long itc_math(string str1, string str2, char oper);
+long long itc_math1(string str);
 #endif // CASE2_H_INCLUDED
