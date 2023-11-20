61af2b6096d822cd8313c00d8c95ff1e13bc0f6f
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-20 13:07:48 +0300

    Update case2.h

--- case2.h
+++ case2.h
@@ -1,8 +1,10 @@
-#ifndef CASE2_H_INCLUDED
-#define CASE2_H_INCLUDED
+#ifndef CASE_H_INCLUDED
+#define CASE_H_INCLUDED
 #include <iostream>
 #include <string>
 using namespace std;
 long long itc_math(string str1, string str2, char oper);
 long long itc_math1(string str);
-#endif // CASE2_H_INCLUDED
+string itc_reverse_str(string str);
+long long itc_len(string str);
+#endif // CASE_H_INCLUDED
