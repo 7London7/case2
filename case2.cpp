8d202f47e640a62f7fea645eb9c0b706c67778de
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-20 13:31:32 +0300

    Update case2.cpp

--- case2.cpp
+++ case2.cpp
@@ -23,6 +23,7 @@ for(long long i = itc_len(str) - 1; i >= 0;i--){
 
 string itc_plus(string str1, string str2){
  long long sum;
+int i = 0;
 string summa;
      itc_reverse_str(str1);
     itc_reverse_str(str2);
@@ -31,15 +32,15 @@ string summa;
 while(sum != 0){
 str2 = str2 + '0';
 }
-while(str1[i] != '\0' && str2[i] != '\0'){
-    summa = str1[i] + str2[i];
-i++;
-}
     }else{
 sum = itc_len(str2) - itc_len(str1);
 while(sum != 0){
 str1 = str1 + '0';
 }
        }
-
+while(str1[i] != '\0' && str2[i] != '\0'){
+    summa = str1[i] + str2[i];
+i++;
+}
+return summa;
 }
