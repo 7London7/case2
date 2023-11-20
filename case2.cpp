4fd464a54dcee2927855a94d7f2393b277a2a979
7London7 <106383366+7London7@users.noreply.github.com>
2023-11-20 11:36:07 +0300

    Update case2.cpp

--- case2.cpp
+++ case2.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include "case2.h"
 using namespace std;
-long long itc_math1(string str){
+/* long long itc_math1(string str){
     int a = 0, st = 1; int i = str.size()-1;
 while(i >= 0){
     a = a +  (str[i] - '0') * st; //1 2 4 8
@@ -11,10 +11,18 @@ while(i >= 0){
 }
 return a;
 }
+*/
 long long itc_math(string str1, string str2, char oper){
  int i = 0;
  int sum;
  if(oper == '+'){
-    sum =  itc_math1(str1) + itc_math1(str2);
+     for(int i = 0; i < str.size(); i++){
+    sum =  (str1[i] - '0') + (str2[i] - '0');
  }
+}
+if(oper == '+'){
+     for(int i = 0; i < str.size(); i++){
+    sum =  (str1[i] - '0') + (str2[i] - '0');
+       }
+    }
 }
